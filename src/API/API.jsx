45eecde4b3da040e1795d7e4b437fc8e05de9f7c
AndreyF <getsuga@tut.by>
2021-12-24 19:23:40 +0300

    Change API file

--- src/API/API.jsx
+++ src/API/API.jsx
@@ -2,13 +2,20 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: "https://api.open-meteo.com/v1/forecast/",
+  withCredentials: true,
   timeout: 1000,
 });
 
 export const weatherAPI = {
   getWeather(latitude, longitude) {
     return instance
-      .get(`latitude=${latitude}&longitude=${longitude}`)
-      .then((response) => response.data);
+      .get(
+        `latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,weathercode,windspeed_10m&current_weather=true&timezone=Europe%2FMoscow`
+      )
+      .then((response) => response.data)
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
   },
 };
