88434d8e7afa03b99c912129108b782b3461cf7e
AndreyF <getsuga@tut.by>
2021-12-26 18:49:12 +0300

    Style refactoring

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.scss";
+import Chart from "./components/Chart";
+import CurrentWeather from "./components/CurrentWeather";
+import Geocodeform from "./components/GeocodeForm";
+import Header from "./components/Header";
 
 function App() {
+  const [hourlyWeather, setHourlyWeather] = useState([]);
+  const [currentWeather, setCurrentWeather] = useState([]);
+  const [isFetching, setFetching] = useState(false);
+  const [isPressure, setPressure] = useState(false);
+  const [isWindspeed, setWindspeed] = useState(false);
+
+  const [latitude, setLatitude] = useState(68.973311);
+  const [longitude, setLongitude] = useState(33.085585);
+
+  console.log(hourlyWeather);
+
+  useEffect(() => {
+    setFetching(true);
+    axios
+      .get(
+        `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,cloudcover,windspeed_10m&current_weather=true&timezone=Europe%2FMoscow&past_days=2`
+      )
+      .then(function (response) {
+        // handle success
+
+        let hourlyWeather = response.data.hourly.time.map(function (i, ind) {
+          return {
+            time: i,
+            temperature: response.data.hourly.temperature_2m[ind],
+            pressure: response.data.hourly.cloudcover[ind],
+            windspeed: response.data.hourly.windspeed_10m[ind],
+          };
+        });
+        setCurrentWeather(response.data.current_weather);
+        setHourlyWeather(hourlyWeather);
+        console.log(hourlyWeather);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function (response) {
+        setFetching(false);
+      });
+  }, [latitude, longitude]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="header">
+        <Header />
       </header>
+      <div className="container">
+        <main className="main">
+          <CurrentWeather
+            time={currentWeather.time}
+            temperature={currentWeather.temperature}
+            weathercode={currentWeather.weathercode}
+            windspeed={currentWeather.windspeed}
+            winddirection={currentWeather.winddirection}
+          />
+          <Geocodeform
+            latitude={latitude}
+            longitude={longitude}
+            setLatitude={setLatitude}
+            setLongitude={setLongitude}
+            setPressure={setPressure}
+            setWindspeed={setWindspeed}
+            isPressure={isPressure}
+            isWindspeed={isWindspeed}
+          />
+
+          <Chart
+            data={hourlyWeather}
+            isFetching={isFetching}
+            currentWeather={currentWeather.time}
+            isPressure={isPressure}
+            isWindspeed={isWindspeed}
+          />
+        </main>
+      </div>
     </div>
   );
 }
--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -41,44 +41,44 @@ const Chart = ({
     return <Preloader />;
   } else
     return (
-      <div className="chart">
-        <h3>Температура</h3>
-        <ResponsiveContainer>
-          <AreaChart
-            // width={1200}
-            // height={700}
-            data={data}
-            margin={{ top: 5, right: 20, bottom: 25, left: 0 }}
-          >
-            <Area
-              type="monotone"
-              dataKey="temperature"
-              stroke="#8884d8"
-              fill="url(#splitColor)"
-            />
-            <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-            <XAxis
-              dy={10}
-              tick={{ fontSize: 12 }}
-              dataKey="time"
-              angle={20}
-              interval="preserveStartEnd"
-            />
-            <YAxis />
-            <ReferenceLine x={currentWeather} stroke="red" label="Сейчас" />
-            <Tooltip />
-            <defs>
-              <linearGradient id="splitColor" x1="0" y1="0" x2="0" y2="1">
-                <stop offset={off} stopColor="red" stopOpacity={1} />
-                <stop offset={off} stopColor="blue" stopOpacity={1} />
-              </linearGradient>
-            </defs>
-          </AreaChart>
-        </ResponsiveContainer>
+      <div>
+        <div className="chart">
+          <h3>Температура</h3>
+          <ResponsiveContainer>
+            <AreaChart
+              data={data}
+              margin={{ top: 5, right: 20, bottom: 25, left: 0 }}
+            >
+              <Area
+                type="monotone"
+                dataKey="temperature"
+                stroke="#8884d8"
+                fill="url(#splitColor)"
+              />
+              <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+              <XAxis
+                dy={10}
+                tick={{ fontSize: 12 }}
+                dataKey="time"
+                angle={20}
+                interval="preserveStartEnd"
+              />
+              <YAxis />
+              <ReferenceLine x={currentWeather} stroke="red" label="Сейчас" />
+              <Tooltip />
+              <defs>
+                <linearGradient id="splitColor" x1="0" y1="0" x2="0" y2="1">
+                  <stop offset={off} stopColor="red" stopOpacity={1} />
+                  <stop offset={off} stopColor="blue" stopOpacity={1} />
+                </linearGradient>
+              </defs>
+            </AreaChart>
+          </ResponsiveContainer>
+        </div>
         {isWindspeed ? (
           <div className="chart">
             <h3>Скорость ветра</h3>
-            <ResponsiveContainer>
+            <ResponsiveContainer width="100%">
               <LineChart
                 data={data}
                 margin={{ top: 5, right: 20, bottom: 25, left: 0 }}
@@ -107,7 +107,7 @@ const Chart = ({
         {isPressure ? (
           <div className="chart">
             <h3>Облачность</h3>
-            <ResponsiveContainer>
+            <ResponsiveContainer width="100%">
               <LineChart
                 data={data}
                 margin={{ top: 5, right: 20, bottom: 25, left: 0 }}
