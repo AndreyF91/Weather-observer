0b0a39650b75cd8f9bf436cba9c67d923f526886
AndreyF <getsuga@tut.by>
2021-12-26 18:30:10 +0300

    Header component refactoring

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ const Header = () => {
   return (
     <div className="header">
       <h1 className="header__title">Weather-Observer</h1>
+      <p>Hourly 7-day forecast worldwide</p>
     </div>
   );
 };
